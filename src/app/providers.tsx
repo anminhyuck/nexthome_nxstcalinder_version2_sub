a60372e286af11548443d5d2a7e7715e03d5480d
anminhyuck <dksals0914@naver.com>
2025-03-05 19:05:41 +0900

    fix: Update providers.tsx to use correct context providers

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,15 +1,24 @@
 'use client';
 
 import React from 'react';
-import { TodoProvider } from './contexts/TodoContext';
 import { CategoryProvider } from './contexts/CategoryContext';
+import { ScheduleProvider } from './contexts/ScheduleContext';
+import { MemoProvider } from './contexts/MemoContext';
+import { AuthProvider } from './contexts/AuthContext';
+import { BookmarkProvider } from './contexts/BookmarkContext';
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
-    <TodoProvider>
-      <CategoryProvider>
-        {children}
-      </CategoryProvider>
-    </TodoProvider>
+    <AuthProvider>
+      <ScheduleProvider>
+        <MemoProvider>
+          <BookmarkProvider>
+            <CategoryProvider>
+              {children}
+            </CategoryProvider>
+          </BookmarkProvider>
+        </MemoProvider>
+      </ScheduleProvider>
+    </AuthProvider>
   );
 } 
